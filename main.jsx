97802dd0e9110a4158b86688a3992f7e621f8f4a
Steve Brownlee <chortlehoort@gmail.com>
2023-10-27 10:53:50 -0500

    Initial structure, styling, and fetch calls complete

--- main.jsx
+++ main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { HomeView } from './views/Home.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <main className='p-5'>
-      <h1 className='text-3xl'>Welcome to Puppy Adoption</h1>
-      <HomeView />
-    </main>
+    <BrowserRouter>
+      <main className='p-5'>
+        <h1 className='text-3xl'>Welcome to Puppy Adoption</h1>
+        <HomeView />
+      </main>
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- views/Home.jsx
+++ views/Home.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 export const HomeView = () => {
     const [allCenters, setCentersArray] = useState([{
@@ -17,14 +18,37 @@ export const HomeView = () => {
         "number_of_puppies": 4
     }])
 
+    const fetchAgencies = async () => {
+        const agencies = await fetch("http://localhost:8000/agencies")
+        const data = await agencies.json()
+        setCentersArray(data)
+    }
+
+    useEffect(() => {
+        fetchAgencies()
+    }, [])
+
     return (
         <>
             <h2>Buford County Adoption Centers</h2>
             {
-                allCenters.map(center => <section className="border border-fuchsia-900 p-8 mt-4 mb-4 bg-yellow-400"
-                                            key={`center-${center.id}`}>
-                    {center.name}
-                </section>)
+                allCenters.map(center => <section className="flex justify-around border border-fuchsia-900 p-8 mt-4 mb-4 bg-yellow-400"
+                    key={`center-${center.id}`}>
+                    <div className="flex-1">
+                        <Link to={`/centers/${center.id}`}
+                            className="text-blue-600 underline hover:text-blue-900"
+                        >
+                            {center.name}
+                        </Link>
+                    </div>
+                    <div className="flex-1">
+                        {center.address}
+                    </div>
+                    <div className="flex-1 flex items-center justify-center">
+                        {center.number_of_puppies}
+                    </div>
+                </section>
+                )
             }
         </>
     )
